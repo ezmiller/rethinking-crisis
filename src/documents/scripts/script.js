55003b20abe26386d0d8909acf54b21aa9f1b2f1
Ethan Miller - Development Server <ethanzanemiller@gmail.com>
2015-04-05 19:04:15 +0200

    SAVEPOINT

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,8 +1,39 @@
-/* Author:
-
+/**
+* Author: Ethan Miller (CodeCuts)
+* scripts.js
 */
+/*jslint browser: true*/
+/*global $, console, Modernizr*/
 
+(function () {
+    'use strict';
 
+    var page = function() {
+        var self = {};
 
+        self.initialize = function() {
+            this.checkFallBacks();
+        };
 
+        self.checkFallBacks = function() {
+            if (!Modernizr.cssvhunit) {
+                $('.intro').height(this.getScreenSize().y);
+            }
+        };
 
+        self.getScreenSize = function () {
+            return {
+                'x': $(window).width(),
+                'y': $(window).height()
+            };
+        };
+
+        return self;
+    };
+
+    window.onload = function() {
+        var view = page();
+        view.initialize();
+    };
+
+}());
