bead1983cde3299b8151a6fe5085477b162c4264
Ethan Zane Miller <ethan@code-cuts.com>
2015-06-19 10:55:55 +0200

    Add smooth scroll that for every link with a scrollTo class

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -28,6 +28,12 @@
                 });
             }.bind(this), 200);
             window.addEventListener('resize', resize);
+
+            $('.scrollTo').on('click', function(e) {
+                e.preventDefault();
+                smoothScroll($(window), $($(e.currentTarget).attr('href')).offset().top, 200);
+            });
+
         };
 
         self.checkFallBacks = function() {
@@ -188,6 +194,20 @@
               return result;
             };
         };
+
+        function smoothScroll(el, to, duration) {
+            if (duration < 0) {
+                return;
+            }
+            var difference = to - $(window).scrollTop();
+            var perTick = difference / duration * 10;
+            this.scrollToTimerCache = setTimeout(function() {
+                if (!isNaN(parseInt(perTick, 10))) {
+                    window.scrollTo(0, $(window).scrollTop() + perTick);
+                    smoothScroll(el, to, duration - 10);
+                }
+            }.bind(this), 10);
+        }
 
         return self;
     };
